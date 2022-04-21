261ab31889896b296d5a4e5326835e8db3d91315
Philips413 <philips413@kakao.com>
2022-04-21 13:55:16 +0900

    feature: 기본적인 구조 설정

---
+++ src/page/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { Button } from "reactstrap";
+import styled from "styled-components";
+
+const MainWrapper = styled.div`
+  
+`
+
+const HomePage = () => {
+    return (
+        <React.Fragment>
+            <Button color={"primary"}>클릭!</Button>
+        </React.Fragment>
+    )
+}
+
+
+export default HomePage;
