70f39f761e2f538508cd758776386b3722d8753a
Philips413 <philips413@kakao.com>
2022-04-23 23:02:05 +0900

    feature: Footer 부분 디자인 적용

--- src/page/layout/Footer.tsx
+++ src/page/layout/Footer.tsx
@@ -1,13 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+
+const FooterDiv = styled.footer`
+  textAlign: "center";
+  div {
+    text-align: center;
+    padding: 20px;
+    background: #333;
+    color: #FFF;
+  }
+  div:first-child {
+    border-right: #555;
+    border-right-style: solid;
+    border-right-width: 1px;
+  }
+  div:last-child {
+    border-left-color: #555;
+    border-left-style: solid;
+    border-left-width: 1px;
+  }
+`
 
 const Footer = () => {
   return (
     <>
-      <footer className={"contentMenu"}>
+      <FooterDiv className={"contentMenu"}>
         <div>HOME</div>
         <div>LIST</div>
         <div>MYPAGE</div>
-      </footer>
+      </FooterDiv>
     </>
   )
 }
