6685a7f0e35ed6b7159905cd3ed5240b267740e4
Philips413 <philips413@kakao.com>
2022-04-27 06:57:52 +0900

    feature: 관리자 로그인

--- src/page/admin/AdminHomePage.tsx
+++ src/page/admin/AdminHomePage.tsx
@@ -1,12 +1,48 @@
 import AdminHeader from "./AdminHeader";
 import {Contents} from "./AdminPage";
+import {Button, Card, CardBody, CardTitle, Form, FormGroup, Input, Label} from "reactstrap";
 
 const AdminHomePage = () => {
   return (
     <>
       <AdminHeader />
       <Contents>
-        TEST
+        <Card>
+          <CardBody>
+            <CardTitle tag={"h4"}>
+              로그인
+            </CardTitle>
+            <Form inline>
+              <FormGroup floating>
+                <Input
+                  id="userId"
+                  name="userId"
+                  placeholder="ID"
+                  type="text"
+                />
+                <Label for="userId">
+                  ID
+                </Label>
+              </FormGroup>
+              {' '}
+              <FormGroup floating>
+                <Input
+                  id="password"
+                  name="password"
+                  placeholder="password"
+                  type="password"
+                />
+                <Label for="password">
+                  Password
+                </Label>
+              </FormGroup>
+              {' '}
+              <Button>
+                로그인
+              </Button>
+            </Form>
+          </CardBody>
+        </Card>
       </Contents>
     </>
   )
