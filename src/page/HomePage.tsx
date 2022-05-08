b0309fe462a2279b53a55c81f282cf67f6417483
Philips413 <philips413@kakao.com>
2022-05-08 23:32:47 +0900

    feature: 장비 신청 화면 수정

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -23,7 +23,7 @@ const HomePage = () => {
   const [isOpenToggle, setIsOpenToggle] = useState(false);
   const [selectItem, setSelectItem] = useState({
     name: "",
-    hasQty: 0,
+    qty: 10,
     propose: "",
     hp: "",
     category: 1
@@ -37,12 +37,18 @@ const HomePage = () => {
   }, [])
 
   const applyEquipmentMethod = (data: EquipmentRequest) => {
-    setSelectItem(data);
+    setSelectItem((props) => ({...props, ...data}));
     doModelToggle();
   }
 
   const doModelToggle = () => (setIsOpenToggle(!isOpenToggle));
-
+  const createOptionDom = ((qty: number) => {
+    let optionDom = [];
+    for(let i = 1 ; i < qty ; i++) {
+      optionDom.push(<option value={i}>{i}</option>)
+    }
+    return optionDom;
+  })
   return (
     <React.Fragment>
       <Contents>
@@ -77,33 +83,27 @@ const HomePage = () => {
         <ModalBody>
           <Form>
             <FormGroup row>
-              <Label
-                for="categoryName"
-                sm={2}
-              >
-                장비명
-              </Label>
+              <Label for="equipmentName" sm={2}>장비명</Label>
               <Col sm={4}>
-                <Input
-                  id="categoryName"
-                  name="categoryName"
-                  type="text"
-                  disabled={true}
-                  value={selectItem.name}
-                />
+                <Input id="equipmentName" name="equipmentName" type="text" disabled={true} value={selectItem.name} />
               </Col>
-              <Label
-                for="description"
-                sm={2}
-              >
-                반출 갯수
-              </Label>
+              <Label for="categoryName" sm={2}>카테고리</Label>
               <Col sm={4}>
-                <Input
-                  id="description"
-                  name="description"
-                  type="text"
-                />
+                {selectItem.category}
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="useQty" sm={2}>반출 갯수</Label>
+              <Col sm={4}>
+                <Input id="useQty" name="useQty" type="select">
+                  {createOptionDom(selectItem.qty)}
+                </Input>
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="propose" sm={2}>목적</Label>
+              <Col sm={10}>
+                <Input id="propose" name="propose" type="textarea"/>
               </Col>
             </FormGroup>
           </Form>
